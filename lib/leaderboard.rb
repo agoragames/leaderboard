eab8123ed71cb1787cb1b75c08a0d6e7b07470b3
David Czarnecki <dczarnecki@agoragames.com>
2011-01-04 09:22:45 -0500

    Added initialize method and test.

--- lib/leaderboard.rb
+++ lib/leaderboard.rb
@@ -1,3 +1,25 @@
+require 'redis'
+
 class Leaderboard
   VERSION = '1.0.0'.freeze
+  
+  def initialize(leaderboard_name, host = 'localhost', port = 6379)
+    @leaderboard_name = leaderboard_name
+    @host = host
+    @port = port
+    
+    @redis_server = Redis.new(:host => @host, :port => @port)
+  end
+  
+  def host
+    @host
+  end
+  
+  def port
+    @port
+  end
+  
+  def leaderboard_name
+    @leaderboard_name
+  end
 end
--- test/test_leaderboard.rb
+++ test/test_leaderboard.rb
@@ -4,4 +4,12 @@ class TestLeaderboard < Test::Unit::TestCase
   def test_version
     assert_equal '1.0.0', Leaderboard::VERSION
   end
+  
+  def test_initialize_with_defaults
+    leaderboard = Leaderboard.new('name')
+    
+    assert_equal 'name', leaderboard.leaderboard_name
+    assert_equal 'localhost', leaderboard.host
+    assert_equal 6379, leaderboard.port
+  end
 end
