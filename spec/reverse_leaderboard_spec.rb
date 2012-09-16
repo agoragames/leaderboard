3a857060e2f8d688a7a948733e1a9535beb5b023
David Czarnecki <czarneckid@acm.org>
2012-09-16 17:58:08 -0400

    Version 3 proposal: Remove use_zero_index_for_rank_option
    Is this ever really used in practice? My guess is no. Although it
    could be left in as it's not necessarily a lot of code, but it
    does remove some conditional paths.

--- spec/reverse_leaderboard_spec.rb
+++ spec/reverse_leaderboard_spec.rb
@@ -16,7 +16,6 @@ describe 'Leaderboard (reverse option)' do
     rank_members_in_leaderboard(5)
 
     @leaderboard.rank_for('member_4').should be(4)
-    @leaderboard.rank_for('member_4', true).should be(3)
   end
 
   it 'should return the correct list when calling leaders' do
@@ -123,7 +122,6 @@ describe 'Leaderboard (reverse option)' do
     @leaderboard.member_at(50)[:rank].should == 50
     @leaderboard.member_at(51).should be_nil
     @leaderboard.member_at(1, :with_member_data => true)[:member_data].should == {'member_name' => 'Leaderboard member 1'}
-    @leaderboard.member_at(1, :use_zero_index_for_rank => true)[:rank].should == 0
   end
 
   it 'should return the correct information when calling around_me' do
@@ -167,7 +165,7 @@ describe 'Leaderboard (reverse option)' do
     @leaderboard.total_members.should be(Leaderboard::DEFAULT_PAGE_SIZE)
     
     members = ['member_1', 'member_5', 'member_10']
-    ranked_members = @leaderboard.ranked_in_list(members, {:with_scores => false, :with_rank => true, :use_zero_index_for_rank => false})
+    ranked_members = @leaderboard.ranked_in_list(members, {:with_scores => false, :with_rank => true})
     
     ranked_members.size.should be(3)
 
