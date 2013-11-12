fc1fb22f746f8b8cdf4c54328c8c41e55249aa2b
David Czarnecki <dczarnecki@agoragames.com>
2013-11-12 12:47:30 -0500

    Bump version to 3.4.0. Update CHANGELOG.

--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe 'Leaderboard::VERSION' do
   it 'should be the correct version' do
-    Leaderboard::VERSION.should == '3.3.0'
+    Leaderboard::VERSION.should == '3.4.0'
   end
 end
